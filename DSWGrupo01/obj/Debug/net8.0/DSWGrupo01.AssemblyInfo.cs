d33eeb48a8d69c1130583a36ac4c68946d574000
osenpb <oeperezbellido@gmail.com>
2025-12-02 18:02:24 -0500

    mejora estilo homepage

--- DSWGrupo01/obj/Debug/net8.0/DSWGrupo01.AssemblyInfo.cs
+++ DSWGrupo01/obj/Debug/net8.0/DSWGrupo01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DSWGrupo01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a90fa6710451aa0f87429b439f07c8740492dce5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+070e9085bde348ed25322cc5adfe84a693a96d7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DSWGrupo01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DSWGrupo01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
